f2408f129ff05cfe4e143d704018bf07e320abd0
Saad Q <mac@Saads-MacBook-Pro.local>
2023-03-22 19:30:47 +0500

    created user schema

---
+++ server/app.js
@@ -0,0 +1,39 @@
+const dotenv = require('dotenv')
+const mongoose = require('mongoose')
+const express = require('express')
+const app = express();
+
+dotenv.config({ path: './config.env' })
+require('./db/conn')
+// const User = require('./model/userSchema');
+
+const PORT = process.env.PORT;
+
+// Middleware
+const middleware = (req, res, next) => {
+    next();
+}
+
+app.get('/', (req, res) => {
+    res.send('Hello world from the server')
+})
+
+app.get('/about', middleware, (req, res) => {
+    res.send('Hello about world from the server')
+})
+
+app.get('/contact', (req, res) => {
+    res.send('Hello contact world from the server')
+})
+
+app.get('/signin', (req, res) => {
+    res.send('Hello signin world from the server')
+})
+
+app.get('/signup', (req, res) => {
+    res.send('Hello signup world from the server')
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running at port no ${PORT}`)
+})
