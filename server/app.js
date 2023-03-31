ebfcf1ab19c56d23ad544c2ef9f91fff871a4679
-g <-g>
2023-03-31 17:04:06 +0500

    Add filter, sort, pagination

--- server/app.js
+++ server/app.js
@@ -7,7 +7,9 @@ dotenv.config({ path: './config.env' })
 require('./db/conn')
 // const User = require('./model/userSchema');
 app.use(express.json());
-app.use(require('./router/auth'))
+app.use(require('./router/auth'));
+const movieRoutes = require("./router/movies");
+app.use("/api", movieRoutes);
 
 const PORT = process.env.PORT;
 
@@ -19,6 +21,7 @@ const middleware = (req, res, next) => {
 app.get('/', (req, res) => {
     res.send('Hello world from the server')
 })
+
 
 app.get('/about', middleware, (req, res) => {
     res.send('Hello about world from the server')
